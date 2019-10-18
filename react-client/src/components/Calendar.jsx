11fdd9966d6ec8451c60d2b33168b827439e5c54
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-10-17 19:23:54 -0700

    Create calendar month components

--- react-client/src/components/Calendar.jsx
+++ react-client/src/components/Calendar.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import CalendarWeek from './CalendarWeek.jsx';
+import CalendarMonth from './CalendarMonth.jsx';
 
 const Calendar = (props) => {
   console.log('props.allMembers', props.allMembers);
+  // send array with dates to calendar month weeks
+  // is there a way to dynamically set those months from the data we get with props?
   return (
   <div>
-    <h4> Calendar Component </h4>
-    {props.allMembers.map(data => (
-      <CalendarWeek member={data} key={data.email} />
-    ))}
+    <div>
+      <h2>October</h2>
+      <CalendarMonth weeks={props} />
+    </div>
+    <div>
+      <h2>November</h2>
+      <CalendarMonth weeks={props} />
+    </div>
+    <div>
+      <h2>December</h2>
+      <CalendarMonth weeks={props} />
+    </div>
+    {/* {props.allMembers.map(data => (
+      <CalendarMonth member={data} key={data.email} />
+    ))} */}
   </div>
   )
 };
---
+++ react-client/src/components/CalendarMonth.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+const CalendarMonth = (props) => {
+  // find out if this month has 4 or 5 weeks and create columns in table accordingly
+  // create script to update this variable to 4 or 5
+  let numWeeks = 4;
+  let createTable = () => {
+    if (numWeeks === 4) {
+      return <table className="months">
+        <tbody>
+          <tr>
+            <td className="date">date 1</td>
+            <td className="date">date 2</td>
+            <td className="date">date 3</td>
+            <td className="date">date 4</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+          </tr>
+        </tbody>
+      </table>
+    } else if (numWeeks === 5) {
+      return <table className="months">
+        <tbody>
+          <tr>
+            <td className="date">date 1</td>
+            <td className="date">date 2</td>
+            <td className="date">date 3</td>
+            <td className="date">date 4</td>
+            <td className="date">date 5</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+            <td>personE</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+            <td>personE</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+            <td>personE</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+            <td>personE</td>
+          </tr>
+          <tr>
+            <td>personA</td>
+            <td>personB</td>
+            <td>personC</td>
+            <td>personD</td>
+            <td>personE</td>
+          </tr>
+        </tbody>
+      </table>
+    }
+  }
+
+  return (
+  <div>
+    {createTable()}
+  </div>
+  );
+}
+
+export default CalendarMonth;
